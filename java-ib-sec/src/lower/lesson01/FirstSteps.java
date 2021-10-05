6ae747a47fd631f0be16d4351b95f41774b8e55e
erikacamilleri <erika.camilleri@gmail.com>
2021-10-05 20:07:28 +0200

    First Steps with Year 12 on 5.10.21

---
+++ java-ib-sec/src/lower/lesson01/FirstSteps.java
@@ -0,0 +1,39 @@
+package lower.lesson01;
+
+public class FirstSteps {
+    
+    public static void example()
+    {
+        // I am just displaying a greeting to the world!
+        System.out.println("Let's declare some variables!");
+        
+        // Declaring an *int* variable (numbers)
+        // Points in a game
+        int points = 45;
+        System.out.println("The value of my points: " + points);
+
+        // Declaring float variable (real numbers)
+        // Storing the temperature data
+        float temperature = (float) 1.1;
+
+        // Declare double variable (real number with a much bigger range)
+        // Total amount of money due to pay a bill
+        double total = 100.00;
+
+        // Declare a char variable (single characters)
+        // Storing a character for a scrabble tile
+        char letter = 'A';
+
+        // Declare a boolean variable (true or false)
+        // A flag to set if something is valid or not valid
+        boolean isValid = true;
+        boolean isYoung = false;
+
+        System.out.println("Let's declare some constants!");
+
+        // Declare a constant
+        // The value of pi does not change in the lifetime of program execution
+        final double PI = 3.142;
+        System.out.println("The value of constant pi in my program: " + PI);
+    }
+}
---
+++ java-ib-sec/src/lower/lesson01/README.md
@@ -0,0 +1,28 @@
+## Your First Steps In Java
+
+Welcome to your first lesson. This lesson is all about breaking the ice and gaining some level of comfort with the basics.
+
+## First Program
+
+Analysis of the Hello, world program
+
+## Comments
+
+- Single line comments
+- Multi-line comments
+
+## Simple Data Types
+
+- int
+- float
+- double
+- char
+- boolean
+
+## Declaration and Assignment of Variables
+
+`int points = 45;`
+
+## Declaration and Assignment of Constants
+
+`final PI  = 3.142;`
