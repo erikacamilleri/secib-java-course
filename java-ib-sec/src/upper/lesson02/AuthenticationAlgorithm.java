0c94b5d9e84a1b9bb2b4240f1768df76d19c1cc1
erikacamilleri <erika.camilleri@gmail.com>
2021-10-26 13:18:12 +0200

    Authentication Algorithm

--- java-ib-sec/src/upper/lesson02/AuthenticationAlgorithm.java
+++ java-ib-sec/src/upper/lesson02/AuthenticationAlgorithm.java
@@ -1,24 +1,37 @@
 package upper.lesson02;
 
+import java.util.Scanner;
+
 public class AuthenticationAlgorithm {
     public static void main(String[] args) {
         // USER_ACCOUNTS // is a collection of user records
         UserAccountRepository userAccounts = new UserAccountRepository();
         userAccounts.addUserAccount(new UserAccount("erika.camilleri@gmail.com", "password123"));
-        
-        
-        
-        
+        userAccounts.addUserAccount(new UserAccount("paul.waterssmith@gmail.com", "password123"));
+        userAccounts.addUserAccount(new UserAccount("jolan.brown@gmail.com", "password123"));
+        userAccounts.addUserAccount(new UserAccount("amy.micallef@gmail.com", "password123"));
+        userAccounts.addUserAccount(new UserAccount("dianne.borg@gmail.com", "password123"));
+
         // INPUT USERNAME
         // INPUT PASSWORD
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please type in your username...");
+        String username = sc.next();
+        System.out.println("Please type in your passw...");
+        String password = sc.next();
         // USER_ACCOUNT = NULL
-        // loop while USER_ACCOUNTS.hasNext() 
-        //     FOUND = false
-        //     if USER_ACCOUNT[POS].username = USERNAME
-        //          USER_ACCOUNT = USER_ACCOUNT[POS]
-        //          FOUND = true
-        //     end if
-        // end loop
+        UserAccount userAccount = userAccounts.findByUsername("paul.waterssmith@gmail.com");
+
+        if (userAccount == null) {
+            System.out.println("There is a problem with your credentials");
+        } else if (userAccount.getPassword() == password) {
+            System.out.println("You are logged in!");
+        } else {
+            System.out.println("There is a problem with your credentials");
+        }
+
+
+        
         // if FOUND = false then
         //     DISPLAY "There is a problem with your credentials."
         // else if PASSWORD = USER_ACCOUNT.password
--- java-ib-sec/src/upper/lesson02/UserAccountRepository.java
+++ java-ib-sec/src/upper/lesson02/UserAccountRepository.java
@@ -1,6 +1,7 @@
 package upper.lesson02;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class UserAccountRepository {
     protected ArrayList<UserAccount> userAccountList;
@@ -18,5 +19,21 @@ public class UserAccountRepository {
      */
     public void addUserAccount(UserAccount userAccount) {
         this.userAccountList.add(userAccount);
+    }
+
+    /**
+     * Please refer to 4.2.2 for standard operations of collections
+     * @param username
+     * @return
+     */
+    public UserAccount findByUsername(String username) {
+        Iterator<UserAccount> it = userAccountList.iterator();
+        while (it.hasNext()) {
+            UserAccount compare = it.next();
+            if (compare.username == username) {
+                return compare;
+            }
+        }
+        return null;
     }
 }
