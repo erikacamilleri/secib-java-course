5038438f2114b7856f6da1e34a3b397f35fbd1fc
erikacamilleri <erika.camilleri@gmail.com>
2021-11-09 08:29:32 +0100

    Recursive fibonacci example

--- java-ib-sec/src/lower/lesson05/MathProblems.java
+++ java-ib-sec/src/lower/lesson05/MathProblems.java
@@ -2,12 +2,21 @@ package lower.lesson05;
 
 public class MathProblems {
     public static void main(String[] args) {
+        /**
+         * Given a number n, square returns n * n
+         */
         int square = MathProblems.square(9, 0, 0);
-        System.out.println(square);
+        System.out.println("Test recursive square n = 9: " + square);
+
+        /**
+         * Given a number n, fibonacciSeries displays fibonacci numbers in sequence up to n
+         */
+        fibonacciSeries(6, 0);
     }
     
     /**
      * Square number from first principles.
+     * https://brilliant.org/courses/math-fundamentals/intro-to-math-fundamentals/introduction/1/
      * @param n
      * @param step
      * @param blocks
@@ -20,6 +29,34 @@ public class MathProblems {
             return MathProblems.square(n, step, blocks);
         } else {
             return blocks;
+        }
+    }
+
+    /**
+     * https://en.wikipedia.org/wiki/Fibonacci_number
+     * @param n
+     * @return
+     */
+    public static int fibonacci(int n) {
+        if (n > 1) {
+            return fibonacci(n-1) + fibonacci(n-2);
+        } else {
+            return n;
+        }
+    }
+
+    /**
+     * https://www.youtube.com/watch?v=wTlw7fNcO-0
+     * @param n
+     * @param step
+     */
+    public static void fibonacciSeries(int n, int step) {
+        
+        System.out.print(fibonacci(step) + " ");
+        
+        if (step < n) {
+            step = step + 1;
+            fibonacciSeries(n, step);
         }
     }
 }
