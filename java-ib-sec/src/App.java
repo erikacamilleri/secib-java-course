0f3fe81a1546d51599bcf1fbf34b41b184a384b0
erikacamilleri <erika.camilleri@gmail.com>
2022-02-28 14:17:22 +0100

    More cleanup

--- java-ib-sec/src/App.java
+++ java-ib-sec/src/App.java
@@ -1,14 +1,4 @@
-import lower.lesson12.PostfixEvaluation;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        PostfixEvaluation p = new PostfixEvaluation();
-        
-        p.expression = "239*+5/";
-
-        int result = p.evaluate();
-
-        System.out.println(result);
     }
 }
