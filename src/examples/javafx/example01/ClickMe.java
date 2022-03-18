bcaf7551dbcdd7b819e1aa5fb8a539c7a36e173f
erikacamilleri <erika.camilleri@gmail.com>
2022-03-18 18:05:44 +0100

    Experiments with JavaFX to look at before recess

---
+++ src/examples/javafx/example01/ClickMe.java
@@ -0,0 +1,47 @@
+package examples.javafx.example01;
+
+import javafx.application.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class ClickMe extends Application {
+    
+    Button btnClickMe;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create the button
+        btnClickMe = new Button();
+        btnClickMe.setText("Click me please!");
+        btnClickMe.setOnAction(e -> onButtonClick());
+
+        // Add the button to the layout panel
+        BorderPane borderLayoutPane = new BorderPane();
+        borderLayoutPane.setCenter(btnClickMe);
+
+        // Add the pane to the scene 
+        Scene btnScene = new Scene(borderLayoutPane, 300, 250);
+
+        // Finalise stage
+        primaryStage.setScene(btnScene);
+        primaryStage.setTitle("Click Me App");
+
+        // Switch on lights
+        primaryStage.show();
+
+    }
+
+    public void onButtonClick() {
+        if (btnClickMe.getText().equals("Click me please!")) {
+            btnClickMe.setText("You clicked me!");
+        } else {
+            btnClickMe.setText("Click me please!");
+        }
+    }
+}
---
+++ src/examples/javafx/example02/ClickCounter.java
@@ -0,0 +1,59 @@
+package examples.javafx.example02;
+
+import javafx.application.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class ClickCounter extends Application {
+    
+    // ---- Data --------------------------------------
+
+    int clickCounter = 0;
+
+    // ---- Components --------------------------------
+
+    Button btnClickMe;
+
+    Label lblClickCount;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create the button
+        btnClickMe = new Button();
+        btnClickMe.setText("Click me please!");
+        btnClickMe.setOnAction(e -> onButtonClick());
+
+        // Create the label
+        lblClickCount = new Label();
+        lblClickCount.setText("Number of clicks: " + clickCounter);
+
+        // Add the label and button to the layout panel
+        BorderPane borderLayoutPane = new BorderPane();
+        borderLayoutPane.setTop(lblClickCount);
+        borderLayoutPane.setCenter(btnClickMe);
+
+        // Add the pane to the scene 
+        Scene btnScene = new Scene(borderLayoutPane, 300, 250);
+
+        // Finalise stage
+        primaryStage.setScene(btnScene);
+        primaryStage.setTitle("Click Counter App");
+
+        // Switch on lights
+        primaryStage.show();
+    }
+
+    public void onButtonClick() {
+        // update data
+        clickCounter++;
+        // update components
+        lblClickCount.setText("Number of clicks: " + clickCounter);
+    }
+}
