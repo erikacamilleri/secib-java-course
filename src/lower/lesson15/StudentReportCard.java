a83f1c5b7c76861fb7116acb0044a506f1cd322e
erikacamilleri <erika.camilleri@gmail.com>
2022-03-29 08:39:22 +0200

    Selection sort for StudentReportCard.sortByGradeSubject()

--- src/lower/lesson15/StudentReportCard.java
+++ src/lower/lesson15/StudentReportCard.java
@@ -66,9 +66,43 @@ public class StudentReportCard {
     }
     
     /**
-     * TODO: Use the selection sort
+     * This method is to sort the grades by subject name in ascending order
+     * Here we are using selection sort
      */
     public void sortByGradeSubject() {
+        for(int current = 0; current <= (this.subjects.length - 2); current++) {
+            int smallestAlphabetically = current;
+            for(int counter = current + 1; counter <= (this.subjects.length - 1); counter++) {
+                int compare = this.subjects[counter].compareTo(this.subjects[smallestAlphabetically]);
+                // if compareTo returns negative, it means that it is smaller
+                if (compare < 0) {
+                    smallestAlphabetically = counter;
+                }
+            }
+            if (smallestAlphabetically != current) {
+                // swap places
+                String tempSubject = this.subjects[smallestAlphabetically];
+                this.subjects[smallestAlphabetically] = this.subjects[current];
+                this.subjects[current] = tempSubject;
 
+                // we also need to perform the swap in questionGrades
+                int rows = this.questionGrades.length;
+                
+                int[][] tempQuestionGrades = new int[rows][1];
+                for (int i = 0; i < rows; i++) {
+                    tempQuestionGrades[i][0] = this.questionGrades[i][smallestAlphabetically];
+                    this.questionGrades[i][smallestAlphabetically] = this.questionGrades[i][current];
+                    this.questionGrades[i][current] = tempQuestionGrades[i][0];
+                }
+            }
+        }
+        // E.g. Math      10    30     40
+        for(int i = 0; i < this.subjects.length; i++) {
+            System.out.print(this.subjects[i] + "      " ); // print in the same line
+            for(int j = 0; j < this.questionGrades.length; j++) {
+                System.out.print(this.questionGrades[j][i] + "    "); // print in the same line
+            }
+            System.out.println(""); // to start a new line
+        }
     }
 }
