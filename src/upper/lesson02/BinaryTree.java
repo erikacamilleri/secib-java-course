221790355038fb7a513dd001da8c2e4bc5364a28
erikacamilleri <erika.camilleri@gmail.com>
2022-10-27 14:00:46 +0200

    Binary Trees Practice

---
+++ src/upper/lesson02/BinaryTree.java
@@ -0,0 +1,10 @@
+package upper.lesson02;
+
+public class BinaryTree {
+
+    Node root;
+    
+    public static void displayPostOrder() {
+
+    }
+}
---
+++ src/upper/lesson02/Node.java
@@ -0,0 +1,14 @@
+package upper.lesson02;
+
+public class Node {
+    
+    String value;
+
+    Node left;
+
+    Node right;
+
+    public Node(String value) {
+        this.value = value;
+    }
+}
