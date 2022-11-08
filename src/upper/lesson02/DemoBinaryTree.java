b1772d9a672b71dd55c85d6a9fdf93a6e04fc7aa
erikacamilleri <erika.camilleri@gmail.com>
2022-11-08 11:51:12 +0100

    Updated Node for practicing recursion in class

--- src/upper/lesson02/DemoBinaryTree.java
+++ src/upper/lesson02/DemoBinaryTree.java
@@ -2,16 +2,33 @@ package upper.lesson02;
 
 public class DemoBinaryTree {
     public static void main(String[] args) {
-        
+        /**
+         * We are creating a Binary Tree of the Year 13s (2023)
+         * Each member of the class is a node...
+         */
         Node finn = new Node("Finn", true);
         Node erika = new Node("Erika", false);
         Node mike = new Node("Mike", false);
         Node anirudh = new Node("Anirudh", false);
+        Node daniil = new Node("Daniil", false);
+        Node bill = new Node("Bill", false);
 
-        erika.setParent(finn, true);
+        // Next, we configure parents and children so that
+        // nodes collectively become a tree
+        daniil.setParent(finn, true);
         mike.setParent(finn, false);
-        anirudh.setParent(erika, true);
+        erika.setParent(daniil, false);
+        bill.setParent(daniil, true);
+        anirudh.setParent(bill, true);
 
-        System.out.println(erika.getLeftChild().visit()); 
+        // Finally, we can invoke a traversing algorithm to visit nodes
+        System.out.print("In Order: ");
+        finn.printInorder();
+
+        // TODO: The following does not work because we have to clean visited nodes first
+        // First implement post order... then use the same logic to implement clean
+        finn.cleanPostOrder();
+        System.out.print("\nPre Order: ");
+        finn.printPreOrder();
     }
 }
--- src/upper/lesson02/Node.java
+++ src/upper/lesson02/Node.java
@@ -2,6 +2,8 @@ package upper.lesson02;
 
 public class Node {
     
+    boolean isVisited;
+
     boolean isRoot;
 
     String value;
@@ -15,6 +17,7 @@ public class Node {
     public Node(String value, boolean isRoot) {
         this.value = value;
         this.isRoot = isRoot;
+        this.isVisited = false;
     }
 
     public void setLeft(Node node) {
@@ -38,6 +41,10 @@ public class Node {
         return this.parent;
     }
 
+    public void setIsVisited(boolean isVisited) {
+        this.isVisited = isVisited;
+    }
+
     public String visit() {
         return this.value;
     }
@@ -48,5 +55,71 @@ public class Node {
 
     public Node getRightChild() {
         return this.right;
+    }
+
+    /**
+     * Tree Node Traversals
+     */
+    public void printInorder()
+    {
+        if (this.isVisited) {
+            return;
+        }
+
+        /* Recur on the left as much as you can */
+        if (this.left != null) {
+            this.left.printInorder();
+        }
+        
+        /* Print */
+        this.isVisited = true;
+        System.out.print(this.visit() + ", ");
+ 
+        /* Recur on the right */
+        if (this.right != null) {
+            this.right.printInorder();
+        }
+    }
+
+    public void printPreOrder() {
+
+        if (this.isVisited) {
+            return;
+        }
+
+        /* First print the value of the node */
+        this.isVisited = true;
+        System.out.print(this.visit() + ", ");
+
+        /* Recur on the left */
+        if(this.left != null) {
+            this.left.printPreOrder();
+        }
+
+        /* Recur on the right */
+        if (this.right != null) {
+            this.right.printPreOrder();
+        }
+    }
+
+    /**
+     * TODO: Visit/print values nodes in post order
+     */
+    public void printPostOrder() {
+
+        // If the node is already visited there return
+
+        /* Recur on the left */
+
+        /* Recur on the right */
+
+        /* Visit and print node value */
+    }
+
+    /**
+     * TODO: Unvisit nodes in post order
+     */
+    public void reset() {
+
     }
 }
